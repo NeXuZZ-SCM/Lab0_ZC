076a8b727f320a7eb7f6d2c07c30b435682e9284
Cristian Martin Sguazzin <34490158+NeXuZZ-SCM@users.noreply.github.com>
2019-05-27 10:55:35 -0300

    Add files via upload

---
+++ VentanaPrincipal/Servicios/S_Permisos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using A_Entidades;
+
+namespace Servicios
+{
+    public class S_Permisos
+    {
+        public static DataTable MostrarPermisos(int id_Usuario)
+        {
+            return Datos.D_Permisos.MostrarPermisos(id_Usuario);
+        }
+        public static DataTable CompletarchklistBoxPermisos()
+        {
+            return Datos.D_Permisos.CompletarchklistBoxPermisos();
+        }
+
+        public static DataTable ConsultarChequeados(int id)
+        {
+            return Datos.D_Permisos.ConsultarChequeados(id);
+        }
+    }
+}
---
+++ VentanaPrincipal/Servicios/S_Usuarios.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class S_Usuarios
+    {
+        #region ABM
+
+        #region Alta
+        public static int AgregarUsuario(A_Entidades.E_Usuario Usuario)
+        {
+            return Datos.D_Usuarios.AgregarUsuario(Usuario);
+        }
+        #endregion
+
+        #region BAJA
+        public static void EliminarUsuario(A_Entidades.E_Usuario usuario)
+        {
+            Datos.D_Usuarios.EliminarUsuario(usuario);
+        }
+        #endregion
+
+        #region Modificacion
+        public static void EditarUsuarios(A_Entidades.E_Usuario usuario)
+        {
+            Datos.D_Usuarios.EditarUsuarios(usuario);
+        }
+        #endregion
+
+
+        #endregion
+
+        #region Show Table
+        public static DataTable obtenerTabla()
+        {
+            return Datos.D_Usuarios.ObtenerTabla();
+        }
+        #endregion
+
+    }
+}
