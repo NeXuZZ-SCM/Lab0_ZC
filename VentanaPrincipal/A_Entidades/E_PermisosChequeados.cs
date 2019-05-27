e7799aab15d4f878a38e80807b536d561fb446b1
Cristian Martin Sguazzin <34490158+NeXuZZ-SCM@users.noreply.github.com>
2019-05-27 10:58:00 -0300

    Add files via upload

---
+++ VentanaPrincipal/A_Entidades/E_PermisosChequeados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Entidades
+{
+    public class E_PermisosChequeados
+    {
+        public int id { get; set; }
+        public string Descripcion { get; set; }
+        public int id_tipo { get; set; }
+        public int Ch { get; set; }
+
+    }
+}
